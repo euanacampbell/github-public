65a55ebcd2c019f394571c28e1c93e1f36652f53
Euan Campbell <euanalisdaircampbell@gmail.com>
2021-06-26 22:53:31 +0100

    test release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # How you named your package folder (MyLib)
     name='gitprofile',
     packages=['gitprofile'],   # Chose the same as "name"
-    version='0.1',      # Start with a small number and increase it with every change you make
+    version='0.2',      # Start with a small number and increase it with every change you make
     # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     license='MIT',
     # Give a short description about your library
