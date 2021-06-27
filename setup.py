9ab25cbc1c8ce03a3920672a39a297786c3b8c92
Euan Campbell <euanalisdaircampbell@gmail.com>
2021-06-27 11:23:39 +0100

    setup fix

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     ],
     classifiers=[
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        '5 - Production/Stable',
+        'Development Status :: 5 - Production/Stable',
         # Define that your audience are developers
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
